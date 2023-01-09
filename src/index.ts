8103a3bfc0a4ab84997c5adbad463980a3f5d3f5
Svenja Widegreen <svenja.widegreen@hausgold.de>
2023-01-09 17:21:02 +0100

    Add books and authors

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,43 @@ import express, {Express, Request, Response} from 'express';
 const app: Express = express()
 const port = 4001
 
+
+const books = [
+    {
+        id: 1,
+        title: 'The Awakening',
+        author: 1,
+    },
+    {
+        id: 2,
+        title: 'City of Glass',
+        author: 2,
+    },
+];
+
+const authors = [
+    {
+        id: 1,
+        name: 'Kate Chopin',
+        books: [1],
+    },
+    {
+        id: 2,
+        name: 'Paul Auster',
+        books: [2],
+    },
+];
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
+})
+
+app.get('/books', (req: Request, res: Response) => {
+    res.json(books);
+})
+
+app.get('/authors', (req: Request, res: Response) => {
+    res.json(authors);
 })
 
 app.listen(port, () => {
